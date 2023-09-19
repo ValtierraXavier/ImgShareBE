d33809f8f55bd48d4c6f25d393ed6aef7319db2b
ValtierraXavier <xavier.valtierra@icloud.com>
2023-09-19 18:32:30 -0400

    add fileUpload support. multi-file next

--- Connection/server.js
+++ Connection/server.js
@@ -3,10 +3,11 @@ import express from 'express'
 import cors from 'cors'
 import routes from '../Router/index.js'
 import chalk from 'chalk'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = process.env.PORT || 3020
-app.use(express.json())
+app.use(express.json({limit: '15mb'}))
 app.use(cors())
 app.use('/', routes)
 
